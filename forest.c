02fa1d853493104083a8d882e22b5a756d5b9652
Philippe Martinet <phi.mar.email@gmail.com>
2021-06-10 21:32:05 +0200

    Test ok for entropy

--- forest.c
+++ forest.c
@@ -8,8 +8,13 @@
 #define N 10
 typedef enum {false,true} bool;
 struct node {
+  int index;
   int data[100];
+  int class[100];
   int ndt;  // number of data
+  int ndc;  // number of classes
+  double entropy;
+
   struct node *left;
   struct node *right;
   struct node *prev;
@@ -18,7 +23,7 @@ struct node {
 };
 
 double entropy(int *,int *,int,int);
-double gain(int *,int *,int,int);
+double gain(int *,int *,int *,int,int);
 double gini(int *,int);
 double prob(int *,int,int);
 void printree(struct node **,int, int);
@@ -49,21 +54,22 @@ int classes(int *X,int *C, int n){
 double entropy(int *p,int *c,int n,int nc){
   // Mesure the non-homogeneity/uncertainly of p probalities
   double h=0,pb;
-  int i,j;
 
-  for(i=0;i<nc;i++){
+  for(int i=0;i<nc;i++){
     pb = prob(p,c[i],n);
-    h += pb*log2(pb);
+    if(pb!= 0.0)
+      h += pb*log2(pb);
   }
+  
   return -h;
 }
 
-double gain(int *X,int *A,int nx,int na){
+double gain(int *X,int *class,int *A,int nx,int na){
   // Gain = Entropy(X) - Conditional entropy(X|A)
   // CondEntropy(Y|X) = - Sum_{x,y}(p(x,y)*log(p(x,y)/p(x)))
   
   double g=0;
-  int class[2] = {1,0};
+
   double hx = entropy(X,class,nx,2);
   double ha = entropy(A,class,na,2);
   
@@ -160,9 +166,9 @@ int main(){
   int T[4][4]={{1,1,0,1},{1,1,0,0},{1,0,1,0},{1,1,2,2}};
 
 #define ND 8
-  int X[ND]={1,0,1,1,0,1,0,1};
-  int class[2]={0,1};
-
+  int X[ND]={1,3,1,1,4,2,1,3,2};
+  int class[3]={1,2,3,4};
+  
 
   struct node *tree[NTREE];
 
@@ -184,7 +190,7 @@ int main(){
 
   // Init all nodes 
   for(i=0;i<NTREE;i++){
-    tree[i]->data[0] = i;
+    tree[i]->index = i;
     int line = i%2 + 1;
     if(i>2 && i<NTREE-1){
       tree[i]->left = tree[2*i+1];
@@ -192,17 +198,29 @@ int main(){
       if(i%2 == 0)
 	tree[i]->parent = tree[i-1]->parent;
       else
-	tree[i]->parent = tree[tree[i-1]->parent->data[0]+1];
+	tree[i]->parent = tree[tree[i-1]->parent->index+1];
       tree[i]->prev = tree[i-1];
       tree[i]->next = tree[i+1];
+      tree[i]->ndt = 0;
+      tree[i]->ndc = 0;
     }else if(i==0){
-      for(int t=0;t<=ND;t++)
-	tree[0]->data[t+1]=X[t];
-
+      for(int t=0;t<ND;t++){
+	tree[0]->data[t]=X[t]; // Populate root with data
+	tree[0]->ndt = ND;
+	tree[0]->ndc = classes(X,class,ND);
+      }
       tree[0]->parent = NULL;
       tree[0]->prev = NULL;
       tree[0]->left = tree[1];
       tree[0]->right = tree[2];
+      // Decision on node 0
+      /*
+      for(int t=0;t<=ND;t++)
+	if(tree[0]->data[t+1]<2)
+	  tree[0]->left->data[t+1]=tree[0]->data[t+1];
+	else
+	  tree[0]->right->data[t+1]=tree[0]->data[t+1];
+      */
       tree[1]->parent = tree[0];
       tree[2]->parent = tree[0];
       tree[0]->next = tree[1];
@@ -210,7 +228,9 @@ int main(){
       if(i%2)
 	tree[i]->parent = tree[i-1]->parent;
       else
-	tree[i]->parent = tree[tree[i-1]->parent->data[0]+1];
+	tree[i]->parent = tree[tree[i-1]->parent->index+1];
+      tree[i]->ndt = 0;
+      tree[i]->ndc = 0;
       tree[i]->next = NULL;
       tree[i]->prev = tree[i-1];
       tree[i]->left = NULL;
@@ -226,27 +246,28 @@ int main(){
   //double X[2]={(double)3/8,(double)5/8};
 
   // Data
-  int C[ND];
-
+  int C[ND]={1,2,3,4};
+  
   // Print data
   for(i=0;i<ND;i++)
     printf("%d ",X[i]);
   printf("\n");
   
-  //  printf("%f=%f ",(double)5/8,prob(X,1,8));
+  //printf("%f=%f ",(double)5/8,prob(X,1,8));
   //printf("%f=%f ",(double)3/8,prob(X,0,8));
-  //printf("%f\n",entropy(X,class,8,2));
+
 
 
   
   int nc=0;
 
   // Find all classes & print them
-  nc = classes(X,C,8);
+  nc = classes(X,C,ND);
   printf("Number of classes %d\n",nc);
   for(i=0;i<nc;i++)
     printf("%d ",C[i]);
 
+  printf("Total entropy: %f\n",entropy(X,C,ND,nc));
   printf("\n");
   // Get the probability for each class & print them
 
@@ -256,30 +277,77 @@ int main(){
     printf("Prob of class %d = %f\n",C[i],P[i]);
   }
 
-  // Calculate total entropi
-  double e = entropy(X,C,ND,nc);
-  printf("Total entropy: %f\n",e);
+
+  double lprob[ND],rprob[ND];
+  int lclass[ND],rclass[ND];
   
   
   // For each possible decisions/branch get the information gain
   // Sorting
-  for(i=0;i<NTREE-1;i++){
-    printf("i=%d\n",i);
-    for(j=1;j<=ND;j++){
-      printf("j=%d\n",j);
-      if(tree[i]->data[j]<1){
-	tree[i]->left->data[j]=tree[i]->data[j];
-	printf("%d ",tree[i]->left->data[j]);
+  for(i=0;i<10;i++){
+    for(int t=0;t<ND;t++){
+      if(tree[i]->left != NULL && tree[i]->data[t] == 1){
+	tree[i]->left->data[tree[i]->left->ndt]=tree[i]->data[t];
+	tree[i]->left->ndt++;
+      }
+      if (tree[i]->right != NULL && tree[i]->data[t] >= 2){
+	tree[i]->right->data[tree[i]->right->ndt]=tree[i]->data[t];
+	tree[i]->right->ndt++;
+      }
+      
+      
+      /*if(tree[i]->data[j]<1){
+	//tree[i]->left->data[j]=tree[i]->data[j];
+	printf("%d ",tree[i]->left->index);
       //=tree[i]->data[1];
       }else{
-	tree[i]->right->data[j]=tree[i]->data[j];
-	printf("%d ",tree[i]->right->data[j]);
+	//tree[i]->right->data[j]=tree[i]->data[j];
+	printf("%d ",tree[i]->right->index);
       //tree[i]->right->data[1]=tree[i]->data[1];
       }
+      */
     }
+
+    //    tree[i]->ndc = classes(tree[i]->data,tree[i]->class,tree[i]->ndt);
+  
+    //double eleft=0,eright=0;
+    //printf("Total entropy: %f\n",tree[0]->entropy);
+
+
+    // Get entropy for left&right childs
+
+    //  if(tree[i]->left != NULL && tree[i]->right != NULL)
+    //  printf("i=%d ndtl=%d ndtr=%d\n",i,tree[i]->left->ndt,tree[i]->right->ndt);
+
+    /*
+    for(int k=0;k<tree[i]->left->ndt;k++)
+      lprob[k] = prob(tree[i]->left->data,tree[i]->left->data[k],tree[i]->left->ndt);
+    for(int k=0;k<tree[i]->right->ndt;k++)
+      rprob[k] =
+      prob(tree[i]->right->data,tree[i]->right->data[k],tree[i]->right->ndt);
+    */
+    
+    //    int ncl = classes(tree[i]->left->data,lclass,tree[i]->left->ndt);
+    //int ncr = classes(tree[i]->right->data,lclass,tree[i]->right->ndt);
+    //printf("nlc=%d ncr=%d",ncl,ncr);
+    //eleft = entropy(lprob,lclass,tree[i]->left->ndt,ncl);
+    //eright = entropy(rprob,rclass,tree[i]->right->ndt,ncr);
+
+  }
+  // Calculate total entropi for each node
+    
+  for(int n=0;n<NTREE;n++){
+    tree[n]->ndc=classes(tree[n]->data,tree[n]->class,tree[n]->ndt);
+    
+    tree[n]->entropy = 0.0;
+    if(tree[n]->ndt > 0)
+      tree[n]->entropy = entropy(tree[n]->data,tree[n]->class,tree[n]->ndt,tree[n]->ndc);
+    printf("(%d,%d,%d) ",n,tree[n]->ndc,tree[n]->class[0]);
   }
 
   printf("\n");
+  //printf("\nleft entropy=%f",eleft);
+  //printf("\nright entropy=%f\n",eright);
   // Find the optimal set of decisiosn
 
 
@@ -298,17 +366,30 @@ int main(){
   printf("%2d%s",1,s);
   while(1){ // while because not all nodes are used
     root = tree[n];  // Pointer to pointer in struc
-    //printf("%x %d %d\n",root,n,root->data[0]);
-    //printf("%d ",root->data[0]);
+    //printf("%x %d %d\n",root,n,root->index);
+    //printf("%d ",root->index);
     strcpy(s," ");
     for(j=0;j<intspc;j++)
       strcat(s," ");
-    //    printf("%x %x %d%s",root,*root,*(*root)->next->data[0],s);
+    //    printf("%x %x %d%s",root,*root,*(*root)->next->index,s);
     //printf("%x %x %d%s",tree[n],root->parent,1,s);
+    // Print parents
+    /*
     if(n>0)
-      printf("(%d,%d)%s",tree[n]->data[0],tree[n]->parent->data[0],s);
+      printf("(%d,%d)%s",tree[n]->index,tree[n]->parent->index,s);
     else
-      printf("(%d,%d)%s",tree[n]->data[0],0,s);
+      printf("(%d,%d)%s",tree[n]->index,0,s);
+    */
+    
+    // Print data
+
+    printf("(%d,",tree[n]->index);
+    for(int t=0;t<ND;t++)
+      if(tree[n]->data[t] != 0)
+	printf("%d",tree[n]->data[t]);
+    printf(",%2.1f)%s",tree[n]->entropy,s);
+
+
     nbe++; // nbr of elements per line
     if(nbe >= nmax){ // nmax max nodes per line
       nl++;
@@ -325,9 +406,6 @@ int main(){
     if(n == NTREE-1) break;
   }
   printf("\n");
-
-
-
 
 
   /*
